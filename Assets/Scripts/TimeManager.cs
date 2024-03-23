6f8b38d3db89632a0fe2a9ff0d6577cdfef8b3b5
keonevos <147702616+keonevos@users.noreply.github.com>
2024-03-23 21:06:49 +0100

    ADD DELAY U DUMBASS

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -15,7 +15,7 @@ public class TimeManager : MonoBehaviour
         Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f); //Le temps va être entre 0 et 1 maximum pour eviter de speed le jeu au dessus de 1
     }
 
-    public void Slowmotion()
+    public void Slowmotion() // add delay / coroutine bcs we can spam it
     {
         Time.timeScale = slowdownFactor;
         Time.fixedDeltaTime = Time.timeScale * 0.2f;
