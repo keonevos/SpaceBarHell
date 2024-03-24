12173006d16455ab48b963045cf42bcf494bb120
keonevos <147702616+keonevos@users.noreply.github.com>
2024-03-24 16:53:26 +0100

    Decay TimeManagement
    Decay added when u hold the space button

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerWeapon : MonoBehaviour
 {
@@ -12,7 +13,7 @@ public class PlayerWeapon : MonoBehaviour
     [SerializeField] private float chargeSpeed = 1f;
     [SerializeField] private float chargeNeeded = 2f;
     [SerializeField] private float chargeTime;
-    private bool isCharging;
+    [SerializeField] private bool isCharging;
     public float spawnCoroutineSecondRate = 1f;
 
     [Header("Enemy")]
@@ -21,6 +22,8 @@ public class PlayerWeapon : MonoBehaviour
     [Header("TimerManagement")]
     public TimeManager TimeManager;
     public Camera m_camera;
+    public float decay = 1f;
+    private float Timedecay;
 
     private void Awake()
     {
@@ -30,12 +33,21 @@ public class PlayerWeapon : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space) && chargeTime < chargeNeeded) 
         {
-            isCharging = true;
-            if (isCharging)
+          
+            isCharging = true;      
+            
+            if (isCharging && Timedecay > 0)
             {
-                TimeManager.Slowmotion();
-                chargeTime += chargeSpeed * Time.deltaTime;
+                Debug.Log("temps");
+                Timedecay -= Time.deltaTime;
             }
+
+            if (isCharging && Timedecay < 0)
+            {
+               TimeManager.Slowmotion();
+               chargeTime += chargeSpeed * Time.deltaTime;
+            }
+            
         }
         if (Input.GetKeyUp(KeyCode.Space))
         {
@@ -45,9 +57,12 @@ public class PlayerWeapon : MonoBehaviour
             }
             else if (chargeTime < chargeNeeded) 
             {
+                Timedecay = decay;
+                isCharging = false;
                 chargeTime = 0f;
             }
         }
+
     }
 
     private void SuperCharge()
@@ -56,6 +71,7 @@ public class PlayerWeapon : MonoBehaviour
         newSuperBullet.Shoot(m_enemy.transform.position);
         isCharging = false;
         chargeTime = 0f;
+        Timedecay = decay;
     }
     IEnumerator Spawner()
     {
