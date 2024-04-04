e0ab6629fca4e206f87ee628a28113ad5f799277
keonevos <147702616+keonevos@users.noreply.github.com>
2024-04-04 19:04:04 +0200

    Pattern (2) boss

--- Assets/Scripts/EnemyWeapon.cs
+++ Assets/Scripts/EnemyWeapon.cs
@@ -1,60 +1,144 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class EnemyWeapon : MonoBehaviour
 {
-    enum SpawnerType { Straight, Spin }
+
+    [Header("Pattern Manager")]
+    private int randPattern;
+    public int secondPattern;
 
     [Header("Bullet Attributes")]
     [SerializeField] private EnemyBullet m_enemybullet;
     public float speed = 1f;
     private float timer = 0f;
 
-    [Header("Spawner Attributes")]
-    [SerializeField] private SpawnerType spawnerType;
-    [SerializeField] private float firingRate = 1f;
 
-    [Header("Decay manager")]
-    private float decay = 0f;
+    [Header("Hell Manager")]
+    public int spinSpeed;
+    public float firingHellRate = 1f;
+    public bool isSpinHellActive = false;
+
+    private float decaySpin = 0f;
     private bool spinSwitch = false;
+
+    [Header("Heaven Manager")]
+    public int spinSpeedHeaven;
+    public float firingRate = 1f;
+    public bool isSpinHeavenActive = false;
+
+    private float decaySpinHeaven = 0f;
+    private bool spinSwitchHeaven = false;
+
 
 
     // Start is called before the first frame update
     // Update is called once per frame
+    private void Start()
+    {
+        StartCoroutine(RandomCheck());
+    }
     void Update()
     {
-        //Debug.Log(decay);
-        timer += Time.deltaTime;
-        if (spawnerType == SpawnerType.Spin && decay >= 0f && spinSwitch == false)
+        
+
+        if (randPattern == 0)
         {
-            decay += Time.deltaTime;
-            transform.eulerAngles = new Vector3(transform.position.x, transform.position.y, transform.eulerAngles.z + 1f);
-            if (decay >= 5f ) 
+            
+            SpinHellPatern();
+        }
+        else { }
+
+        if (randPattern == 1)
+        {
+            
+            SpinHeavenPatern();
+        }
+        else { }
+        
+    }
+
+    private IEnumerator RandomCheck()
+    {
+        print(randPattern);
+        randPattern = Random.Range(0, 2);
+        yield return new WaitForSeconds(secondPattern);
+        StartCoroutine(RandomCheck());
+    }
+    private void SpinHellPatern()
+    {
+        timer += Time.deltaTime;
+        if (decaySpin >= 0f && spinSwitch == false)
+        {
+            decaySpin += Time.deltaTime;
+            transform.eulerAngles = new Vector3(transform.position.x, transform.position.y, transform.eulerAngles.z + spinSpeed);
+            if (decaySpin >= 5f)
             {
                 spinSwitch = true;
             }
         }
 
-        if (spawnerType == SpawnerType.Spin && spinSwitch == true )
+        if (spinSwitch == true)
         {
-            decay += Time.deltaTime;
-            transform.eulerAngles = new Vector3(transform.position.x, transform.position.y, transform.eulerAngles.z - 1f);
-            if (decay >= 10f )
+            decaySpin += Time.deltaTime;
+            transform.eulerAngles = new Vector3(transform.position.x, transform.position.y, transform.eulerAngles.z - spinSpeed);
+            if (decaySpin >= 10f)
             {
                 spinSwitch = false;
-                decay = 0f;
+                decaySpin = 0f;
             }
         }
 
-        if (timer >= firingRate)
+        if (timer >= firingHellRate)
         {
             Fire();
             timer = 0;
         }
     }
+    private void SpinHeavenPatern()
+    {
+            timer += Time.deltaTime;
+            if (decaySpinHeaven >= 0f && spinSwitchHeaven == false)
+            {
+            decaySpinHeaven += Time.deltaTime;
+                transform.eulerAngles = new Vector3(transform.position.x, transform.position.y, transform.eulerAngles.z + spinSpeed);
+                if (decaySpinHeaven >= 5f)
+                {
+                spinSwitchHeaven = true;
+                }
+            }
+
+            if (spinSwitchHeaven == true)
+            {
+            decaySpinHeaven += Time.deltaTime;
+                transform.eulerAngles = new Vector3(transform.position.x, transform.position.y, transform.eulerAngles.z - spinSpeed);
+                if (decaySpinHeaven >= 10f)
+                {
+                spinSwitchHeaven = false;
+                decaySpinHeaven = 0f;
+                }
+            }
+
+            if (timer >= firingRate)
+            {
+                Fire2();
+                timer = 0;
+            }
+        }
     private void Fire()
+    {
+        if (m_enemybullet)
+        {
+            EnemyBullet newenemyBullet = Instantiate(m_enemybullet, transform.position, transform.rotation);
+            newenemyBullet.GetComponent<EnemyBullet>().bulletSpeed = speed;
+            newenemyBullet.transform.rotation = transform.rotation;
+        }
+    }
+    private void Fire2()
     {
         if (m_enemybullet)
         {
